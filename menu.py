9ea42e8205afeb1c92f64bcb8738dd0ad3d38536
ariel <ariel.pujol@ynov.com>
2024-03-12 12:27:12 +0100

    Ship V1

--- menu.py
+++ menu.py
@@ -1,4 +1,5 @@
 from game import Game
+from tkinter import *
 
 class Menu:
 
--- ships/ship.py
+++ ships/ship.py
@@ -1,2 +1,28 @@
 class Ship:
-    pass
+
+    def __init__(self, lenght, height):
+        self.lenght = lenght
+        self.height = height
+        self.isSunk = False
+        self.isPlaced = False
+
+    def getLenght(self):
+        return self.lenght
+    
+    def getHeight(self):
+        return self.height
+    
+    def getIsSunk(self):
+        return self.isSunk
+    
+    def setIsSunk(self, newIsSunk):
+        self.isSunk = newIsSunk
+
+    def getIsPlaced(self):
+        return self.isPlaced
+    
+    def setIsPlaced(self, newIsPlaced):
+        self.isPlaced = newIsPlaced
+
+    def showInfo(self):
+        print(f"{self.isSunk} ,{self.height} ,{self.lenght},{self.isPlaced}")   
