11919389e23dc15006862a79652613fbda89e813
rimworlds <ethan.capus@outlook.fr>
2024-03-15 10:36:34 +0100

    started Board class wit a constructor and a func that changes values on the board

--- board.py
+++ board.py
@@ -1,2 +1,20 @@
+import numpy as np
+
+# playzone can have only 4 kind of value:
+#   0 : nothing present
+#   1 : boat present
+#   2 : miss
+#   3 : hit
+
 class Board:
-    pass
+    
+    def __init__(self):
+        self.playzone = np.zeros(15, 15)
+        
+
+def changeBordSituation(self, coordWherelanded):
+    if (self.playzone[coordWherelanded[0], coordWherelanded[1]] == 0):
+        self.playzone[coordWherelanded[0], coordWherelanded[1]] = 2
+    elif (self.playzone[coordWherelanded[0], coordWherelanded[1]] == 1):
+        self.playzone[coordWherelanded[0], coordWherelanded[1]] = 3
+
