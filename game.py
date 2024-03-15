96f66c4f45e0852cc8d45e2142f305bf2ccd9c10
rimworlds <ethan.capus@outlook.fr>
2024-03-15 11:50:15 +0100

    added constuctor for game class

--- game.py
+++ game.py
@@ -1,4 +1,13 @@
 from board import Board
+from fleat import Fleat
 
 class Game:
-    pass
+    
+    def __init__(self, isAiOn):
+        self.P1Board = Board()
+        self.p2Board = Board()
+
+        self.p1Fleat = Fleat()
+        self.p2Fleat = Fleat()
+
+        self.isAiOn = isAiOn
