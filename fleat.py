26f9dd2b6c107db0e36f258633b6efd9baa178a3
rimworlds <ethan.capus@outlook.fr>
2024-03-15 11:43:52 +0100

    added constuctor for fleat and function to verify if the fleat is destroyed

--- fleat.py
+++ fleat.py
@@ -1,2 +1,19 @@
+import ships.aircraftCarrier as AC
+import ships.cruiser as CR
+import ships.destroyer as DS
+import ships.submarine as SU
+
 class Fleat:
-    pass
+    
+    def __init__(self):
+        self.airCarry = AC()
+        self.cruser = CR()
+        self.destroy1 = DS()
+        self.destroy2 = DS()
+        self.sub1 = SU()
+        self.sub2 = SU()
+
+    def isFleatDestroyed(self):
+        if (self.airCarry.getIsSunk() and self.cruser.getIsSunk and self.destroy1.getIsSunk and self.destroy2.getIsSunk and self.sub1.getIsSunk and self.sub2.getIsSunk):
+            return True
+        return False
