556fb2696768f2af7be0612888891cb989250b69
rimworlds <ethan.capus@outlook.fr>
2024-03-15 12:19:06 +0100

    added isValidShot to test if the shot is valid (not on a shot)

--- board.py
+++ board.py
@@ -9,12 +9,22 @@ import numpy as np
 class Board:
     
     def __init__(self):
-        self.playzone = np.zeros(10, 10)
-        
+        self.playzone = np.zeros(10, 10)      
 
-def changeBordSituation(self, coordWherelanded):
-    if (self.playzone[coordWherelanded[0], coordWherelanded[1]] == 0):
-        self.playzone[coordWherelanded[0], coordWherelanded[1]] = 2
-    elif (self.playzone[coordWherelanded[0], coordWherelanded[1]] == 1):
-        self.playzone[coordWherelanded[0], coordWherelanded[1]] = 3
+    def changeBordSituation(self, coordWherelanded):
+        if (self.playzone[coordWherelanded[0], coordWherelanded[1]] == 0):
+            self.playzone[coordWherelanded[0], coordWherelanded[1]] = 2
+        elif (self.playzone[coordWherelanded[0], coordWherelanded[1]] == 1):
+            self.playzone[coordWherelanded[0], coordWherelanded[1]] = 3
+
+    def isValdeShot(self, coords):
+        if (self.playzone[coordWherelanded[0], coordWherelanded[1]] == 0):
+            return True
+        elif (self.playzone[coordWherelanded[0], coordWherelanded[1]] == 1):
+            return True
+        elif (self.playzone[coordWherelanded[0], coordWherelanded[1]] == 2):
+            return False
+        elif (self.playzone[coordWherelanded[0], coordWherelanded[1]] == 3):
+            return False
+
 
