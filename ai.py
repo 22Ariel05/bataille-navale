0c03fc3ef96b43bd4eb28d1e3f4652259e4e8edd
rimworlds <ethan.capus@outlook.fr>
2024-03-29 09:19:18 +0100

    started the ai class, added: InitialiseAiBoard, PlaceFleat & SelectShot

--- ai.py
+++ ai.py
@@ -21,13 +21,13 @@ class Ai:
                     valid_placement = True
 
 
-    def selectShot(self, opponent_board):
+    def selectShot(self):
         # Exemple de sélection de tir aléatoire
         while True:
             row = random.randint(0, 9)
             col = random.randint(0, 9)
             if (row, col) not in self.previousShots:
                 self.previousShots.append((row, col))
-                return row, col
+                return [row, col]
 
     
